f2e87f60408d8301e4842f47ab5d052d081e806f
codewithashim <codewithashim@gmail.com>
2022-12-27 03:32:09 +0600

    added signup

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import AdminMain from "../Components/Admin/AdminMain";
 import FontPage from "../Components/FontPage/FontPage";
-import Login from "../Auth/Login/Login.js";
+import Login from "../Auth/Login/Login";
 import SubadminMain from "../Components/Subadmin/SubadminMain";
 import UserMain from "../Components/User/UserMain";
+import Register from "../Auth/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/register",
+    element: <Register></Register>,
   },
   {
     path: "/admin",
