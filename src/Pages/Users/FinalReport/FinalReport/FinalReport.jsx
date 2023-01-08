d28f7efb4e08e47bdcbd803ea16a8f708103baf6
codewithashim <codewithashim@gmail.com>
2023-01-08 22:50:01 +0600

    finaly final report page done

--- src/Pages/Users/FinalReport/FinalReport/FinalReport.jsx
+++ src/Pages/Users/FinalReport/FinalReport/FinalReport.jsx
@@ -3,6 +3,7 @@ import ListOfFinalReport from "../ListOfFinalReport/ListOfFinalReport";
 import { Link } from "react-router-dom";
 import useFinalReport from "../FinalReportHooks/useFinalReport";
 import FinalReportForm from "../FinalReportForm/FinalReportForm";
+import FinalReportStockExcessForm from "../FinalReportStockExcessForm/FinalReportStockExcessForm";
 
 const FinalReport = () => {
   const {
@@ -11,6 +12,10 @@ const FinalReport = () => {
     addOneFinalReport,
     handelOnChangeFinalReport,
     handelOnSubmitFinalReport,
+    handelOnChangeStockExcess,
+    finalReportSockExcessState,
+    addOneStockExcess,
+    addFiveStockExcess,
   } = useFinalReport();
 
   return (
@@ -60,13 +65,6 @@ const FinalReport = () => {
           <div>
             <div className="mt-4 flex gap-4">
               <button
-                className="dailyReportBtnSubmit"
-                onClick={() => handelOnSubmitFinalReport()}
-                type="submit"
-              >
-                Submit
-              </button>
-              <button
                 className="dailyReportBtn"
                 onClick={() => addFiveFinalReeport()}
               >
@@ -75,6 +73,67 @@ const FinalReport = () => {
               <button
                 className="dailyReportBtn"
                 onClick={() => addOneFinalReport()}
+              >
+                ADD 1
+              </button>
+              <Link className="dailyReportBtn text-center flex justify-center items-center">
+                सूची
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-4">
+        <div>
+          <h2 className="font-bold text-[1.5rem] titleStyle">माल नामे</h2>
+        </div>
+        <div>
+          <form action="">
+            <div className="overflow-x-auto">
+              <table className="table w-full">
+                <thead>
+                  <tr>
+                    <th>S.no</th>
+                    <th>पार्टी का नाम</th>
+                    <th>राण्ड</th>
+                    <th>संख्या</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {finalReportSockExcessState.map((StockExcess, index) => {
+                    return (
+                      <FinalReportStockExcessForm
+                        key={index}
+                        index={index}
+                        StockExcess={StockExcess}
+                        handelOnChangeStockExcess={handelOnChangeStockExcess}
+                      ></FinalReportStockExcessForm>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </form>{" "}
+          <div>
+            <div className="mt-4 flex gap-4">
+              <button
+                className="dailyReportBtnSubmit"
+                onClick={() => handelOnSubmitFinalReport()}
+                type="submit"
+              >
+                Submit
+              </button>
+              <button
+                className="dailyReportBtn"
+                onClick={() => addFiveStockExcess()}
+              >
+                ADD 5
+              </button>
+              <button
+                className="dailyReportBtn"
+                onClick={() => addOneStockExcess()}
               >
                 ADD 1
               </button>
--- src/Pages/Users/FinalReport/FinalReportHooks/useFinalReport.jsx
+++ src/Pages/Users/FinalReport/FinalReportHooks/useFinalReport.jsx
@@ -6,6 +6,11 @@ const useFinalReport = () => {
     brand: "",
     theNumber: "",
   };
+
+  const [finalReportSockExcessState, setFinalReportSockExcessState] = useState([
+    useFinalReport,
+  ]);
+
   const [finalReportState, setFinalReportState] = useState([finalReportData]);
 
   const addFiveFinalReeport = () => {
@@ -35,6 +40,35 @@ const useFinalReport = () => {
     ]);
   };
 
+  // ================== Stock Excess ==================
+
+  const addFiveStockExcess = () => {
+    let data = finalReportSockExcessState;
+
+    for (let i = 0; i < 5; i++) {
+      data = [
+        ...data,
+        {
+          partyName: "",
+          brand: "",
+          theNumber: 0,
+        },
+      ];
+    }
+    setFinalReportSockExcessState(data);
+  };
+
+  const addOneStockExcess = () => {
+    setFinalReportSockExcessState([
+      ...finalReportSockExcessState,
+      {
+        partyName: "",
+        brand: "",
+        theNumber: 0,
+      },
+    ]);
+  };
+
   const handelOnChangeFinalReport = (e, index) => {
     const finalReportHandel = finalReportState.map((Partner, i) => {
       if (index === i) {
@@ -46,17 +80,34 @@ const useFinalReport = () => {
     setFinalReportState(finalReportHandel);
   };
 
+  const handelOnChangeStockExcess = (e, index) => {
+    const finalReportHandel = finalReportSockExcessState.map((Partner, i) => {
+      if (index === i) {
+        return Object.assign(Partner, { [e.target.name]: e.target.value });
+      } else {
+        return Partner;
+      }
+    });
+    setFinalReportSockExcessState(finalReportHandel);
+  };
+
+  const finalSumit = finalReportData + finalReportSockExcessState;
+
   const handelOnSubmitFinalReport = (e) => {
-    const handelFinalReport = Object.assign({}, finalReportState);
+    const handelFinalReport = Object.assign({}, finalSumit);
     console.log(handelFinalReport);
   };
 
   return {
     finalReportState,
+    finalReportSockExcessState,
     addFiveFinalReeport,
     addOneFinalReport,
     handelOnChangeFinalReport,
     handelOnSubmitFinalReport,
+    handelOnChangeStockExcess,
+    addOneStockExcess,
+    addFiveStockExcess,
   };
 };
 
