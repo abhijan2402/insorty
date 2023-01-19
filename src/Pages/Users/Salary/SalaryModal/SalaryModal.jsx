16196ad91b69c6b0edbcf08cdd7d4f8503d39534
codewithashim <codewithashim@gmail.com>
2023-01-19 13:07:14 +0600

    fixed the error

--- src/Pages/Users/Salary/SalaryModal/SalaryModal.jsx
+++ src/Pages/Users/Salary/SalaryModal/SalaryModal.jsx
@@ -8,11 +8,10 @@ const SalaryModal = ({
 }) => {
   return (
     <>
-      {/* The button to open modal */}
-
       {/* Put this part before </body> tag */}
 
       <input type="checkbox" id="addData" className="modal-toggle" />
+
       <div className="modal">
         <div className="modal-box w-2/3 max-w-5xl">
           <h3 className="font-bold text-lg text-center">Add New</h3>
@@ -34,7 +33,7 @@ const SalaryModal = ({
 
                 <div className="input-group">
                   <input
-                    type="date"
+                    type="number"
                     name="salary_monthYear"
                     value={salaryState.salary_monthYear}
                     onChange={(e) => handelSelaryOnChange(e)}
@@ -70,7 +69,9 @@ const SalaryModal = ({
 
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text font-bold text-[1rem] mt-3">Payment / भुगतान</span>
+                  <span className="label-text font-bold text-[1rem] mt-3">
+                    Payment / भुगतान
+                  </span>
                 </label>
               </div>
               <div className="form-control">
@@ -132,48 +133,48 @@ const SalaryModal = ({
                   />
                 </div>
               </div>
-            </form>{" "}
-            <div className="mt-2">
-              {isLoading ? (
-                <>
-                  <button
-                    type="button"
-                    className="inline-flex items-center px-4 py-2 text-sm font-semibold leading-6 text-[#AA237A] transition duration-150 ease-in-out border-2 border-[#AA237A] rounded-md shadow cursor-not-allowed"
-                    disabled=""
-                  >
-                    <svg
-                      className="w-5 h-5 mr-3 -ml-1 text-[#AA237A] animate-spin"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
+              <div className="mt-2">
+                {isLoading ? (
+                  <>
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-4 py-2 text-sm font-semibold leading-6 text-[#AA237A] transition duration-150 ease-in-out border-2 border-[#AA237A] rounded-md shadow cursor-not-allowed"
+                      disabled=""
                     >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        stroke-width="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
-                    Loading...
+                      <svg
+                        className="w-5 h-5 mr-3 -ml-1 text-[#AA237A] animate-spin"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          stroke-width="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                      Loading...
+                    </button>
+                  </>
+                ) : (
+                  <button
+                    className="dailyReportBtnSubmit"
+                    onClick={() => handelSalaryOnSubmit()}
+                    type="submit"
+                  >
+                    Submit
                   </button>
-                </>
-              ) : (
-                <button
-                  className="dailyReportBtnSubmit"
-                  onClick={() => handelSalaryOnSubmit()}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              )}
-            </div>
+                )}
+              </div>
+            </form>{" "}
           </div>
 
           <div className="modal-action">
