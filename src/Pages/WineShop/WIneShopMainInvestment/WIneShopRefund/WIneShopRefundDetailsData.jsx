970db85c092375750973ae59441d025381e91820
codewithashim <codewithashim@gmail.com>
2023-01-31 21:01:38 +0600

    error fixed

--- src/Pages/WineShop/WIneShopMainInvestment/WIneShopRefund/WIneShopRefundDetailsData.jsx
+++ src/Pages/WineShop/WIneShopMainInvestment/WIneShopRefund/WIneShopRefundDetailsData.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../UserStyle/UserStyle.scss";
+import "../../WineShopUserStyle/UserStyle.scss";
 import { Link } from "react-router-dom";
 import useMainInvestmentHooks from "../WineShopMainInvestmentHooks/useWineShopMainInvestmentHooks";
 
