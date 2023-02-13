b56bb57d4221ff7a3c4cbe5e4c94646d3e892b47
codewithashim <codewithashim@gmail.com>
2023-02-13 16:16:48 +0600

    desing is complited

---
+++ src/Pages/Users/DailyReport/DetailsReports/FullDetailsReport/BackDetailReport/InflowBorrow/InflowBorrow.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const InflowBorrow = () => {
+  return (
+    <>
+      <td className="tg-0lax">1</td>
+      <td className="tg-0lax" colSpan={4}>
+        data 1
+      </td>
+      <td className="tg-0lax" colSpan={4}>
+        data 2
+      </td>
+      <td className="tg-0lax" colSpan={4}>
+        data 3
+      </td>
+      <td className="tg-0lax" colSpan={4}>
+        data 4
+      </td>
+    </>
+  );
+};
+
+export default InflowBorrow;
---
+++ src/Pages/Users/DailyReport/DetailsReports/FullDetailsReport/BackDetailReport/ShippingEnglishBear/ShippingEnglishBear.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ShippingEnglishBear = () => {
+  return (
+    <>
+      <tr>
+        <td className="tg-0lax">1</td>
+        <td className="tg-0lax" colSpan={4}>
+          Data 1
+        </td>
+        <td className="tg-0lax" colSpan={4}>
+          Data 2
+        </td>
+        <td className="tg-0lax" colSpan={4}>
+          Data 3
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default ShippingEnglishBear;
